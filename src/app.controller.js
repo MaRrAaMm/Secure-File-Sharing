b7b0494e8e4af7df498109e0cdcebc99ebcdb575
maram <maram@gmail.com>
2026-03-24 21:39:19 +0200

    second

--- src/app.controller.js
+++ src/app.controller.js
@@ -21,7 +21,8 @@ const bootstrap = async(app, express)=>{
   app.use(helmet());
   //app.use(cors({origin:process.env.ALLOWED_ORIGIN ||"*"}));
   app.use(cors());
-  app.use(express.json());
+  app.use(express.json({limit:'50mb'}));
+  app.use(express.urlencoded({extended:true, limit:'50mb'}));
   await connectDB();
 
   app.use("/auth", authLimiter, authRouter);
