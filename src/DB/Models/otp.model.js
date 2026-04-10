0a21b1bd87342da22378a5e03e58f7c96f8dff99
maram <maram@gmail.com>
2026-04-10 14:45:01 +0200

    second

--- src/DB/Models/otp.model.js
+++ src/DB/Models/otp.model.js
@@ -1,27 +1,32 @@
-import{ Schema, model } from "mongoose";
+import { Schema, model } from "mongoose";
+
 const otpSchema = new Schema(
   {
-    userId:{
-      type:Schema.Types.ObjectId,
-      ref:"User",
-      required:true,
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    code:{
-      type:String,
-      required:true,
+
+    code: {
+      type: String,
+      required: true,
     },
-    type:{
-      type:String,
-      enum:["confirmEmail","resetPassword"],
-      required:true,
+
+    type: {
+      type: String,
+      enum: ["confirmEmail", "resetPassword"],
+      required: true,
     },
-    expiresAt:{
-      type:Date,
-      required:true,
+
+    expiresAt: {
+      type: Date,
+      required: true,
     },
   },
-{timestamps: true}
+  { timestamps: true }
 );
 
-otpSchema.index({expiresAt:1},{expireAfterSeconds:0});
+otpSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+
 export const OTP = model("OTP", otpSchema);
